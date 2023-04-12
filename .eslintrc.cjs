84387f125c46a740a42080444c2e7e96b42611a2
Alaba Samuel Ayomide <alabasamuelayomide3@gmail.com>
2023-04-12 06:45:36 +0100

    setup eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,55 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+		'vue/no-v-html': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-tabs': 'off',
+		'no-unused-vars': 'off',
+		'vue/no-multiple-template-root': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'import/no-named-as-default': 'off',
+		'vue/no-template-shadow': 'off',
+		'no-var': 'error',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'unused-import': 'off',
+		'space-before-function-paren': 'off',
+		camelcase: 'off',
+		'accessor-pairs': 'off',
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		indent: ['off', 'tab', { SwitchCase: 1 }],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		semi: ['error', 'never'],
+		quotes: ['error', 'single'],
+		'prefer-const': ['error'],
+		'arrow-parens': ['error', 'always'],
+		'no-return-assign': 'off',
+		'vue/multi-word-component-names': 'off',
+		curly: 'off',
+		'vue/html-indent': [
+			'warn',
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: []
+			}
+		],
+		'vue/no-mutating-props': 'off',
+		'object-property-newline': 'off',
+		'require-atomic-updates': 'off',
+		'require-await': 'off'
+	},
 }
